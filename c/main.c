dff488a52df92dab8467022e3975a0c4fd31cbff
david_ferrari <ferraridavid.hub@gmail.com>
2025-08-12 18:48:45 +0200

    Analyzing the average time of insertion operation
    - Empirically validates O(1 + α) insertion complexity, where
    α is the load factor

--- c/main.c
+++ c/main.c
@@ -1,36 +1,94 @@
+#include<math.h>
 #include<stdlib.h>
 #include<stdio.h>
+
+#include<time.h>
 
 #include "ht/hash_table.h"
 #include "utils/hash_table_utils.h"
 
+#define INSERTIONS 50000
+#define TEST_ITER 20
+
+
+/*
+ * Elapsed time in nanoseconds
+ */
+long long timediff(struct timespec* end, struct timespec* start) {
+    return (end->tv_sec - start->tv_sec) * 1000000000LL + 
+        (end->tv_nsec - start->tv_nsec);
+}
+
+
+double ratio_avg(size_t size, double ratio[static size]) {
+    double avg = 0;
+    for (size_t i = 0; i < size; i++) {
+        avg += ratio[i];
+    }
+    return avg / size;
+}
+
+
+double ratio_std(size_t size, double ratio[static size]) {
+    double std = 0;
+    double avg = ratio_avg(size, ratio);
+
+    for (size_t i = 0; i < size; i++) {
+        std = pow(ratio[i] - avg, 2);
+    }
+
+    return sqrt(std / size);
+}
+
+
+void init_ratio_vector(size_t size, double ratio[static size]) {
+    for (size_t i = 0; i < size; i++) {
+        ratio[i] = 0.0;
+    }
+}
+
+
+void normalize_ratio_vector(size_t size, double ratio[static size]) {
+    for (size_t i = 0; i < INSERTIONS; i++) {
+        ratio[i] /= TEST_ITER;
+    }
+}
+
 
 int main(void) {
-    HashTable ht;
-    ht_init(&ht);
 
-    // this will cause a leak 
-    char* str = malloc(sizeof(char) * 5);
-    str[0] = 'a';
-    str[1] = 'a';
-    str[2] = 'a';
-    str[3] = 'a';
-    str[4] = 0;
+    struct timespec start, end;
 
-    ht_insert(&ht, 25, str);
-    ht_insert(&ht, 25, "sanvi");
-    ht_insert(&ht, 324, "consomdsf");
-    ht_insert(&ht, 234, "c programming");
-    ht_insert(&ht, 3240, "hash_table");
+    double ratio[INSERTIONS];
 
-    ht_print(&ht);
+    init_ratio_vector(INSERTIONS, ratio);
 
-    ht_delete(&ht, 25);
-    ht_delete(&ht, 26);
+    for (int j = 0; j < TEST_ITER; j++) {
+        HashTable ht;
+        ht_init(&ht);
+        for (int i = 0; i < INSERTIONS; i++) {
+            clock_gettime(CLOCK_MONOTONIC, &start);
 
-    ht_print(&ht);
+            ht_insert(&ht, i, "");
 
-    ht_free(&ht);
-    free(str);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            long long insertion_time = timediff(&end, &start);
+            double load_factor = (double)i / TABLE_SIZE;
+            double current_ratio = (double)insertion_time / (load_factor + 1);
+
+            ratio[i] += current_ratio;
+        }
+        ht_free(&ht);
+    }
+
+    normalize_ratio_vector(INSERTIONS, ratio);
+
+
+    double average = ratio_avg(INSERTIONS, ratio);
+    double std = ratio_std(INSERTIONS, ratio);
+
+    printf("Average ratio: %f\tStandard deviation ratio: %f\n", average, std);
+
 }
 
